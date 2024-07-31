c2c41772789df28602ecd93c8848d2e362462386
girish1012 <girish8012@gmail.com>
2024-07-31 09:34:55 +0000

    Update main6.tf

--- main.tf
+++ main.tf
@@ -21,13 +21,13 @@ resource "aws_key_pair" "tf-key-pair24" {
 
 key_name = var.keyname
 
-public_key = tls_private_key.rsa.public_key_openssh
+public_key = tls_private_key.rsa24.public_key_openssh
 
 }
 
 resource "local_file" "tf-key24" {
 
-content = tls_private_key.rsa.private_key_pem
+content = tls_private_key.rsa24.private_key_pem
 
 filename = var.keyname
 
